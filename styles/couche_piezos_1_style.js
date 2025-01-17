9c3cf5886dc54077c480a81958142efc79ded426
eperaud <148035173+eperaud@users.noreply.github.com>
2025-01-17 17:08:11 +0100

    Add files via upload

---
+++ styles/couche_piezos_1_style.js
@@ -0,0 +1,115 @@
+var size = 0;
+var placement = 'point';
+function categories_couche_piezos_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'A venir':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(198,36,238,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Auto':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(25,33,247,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Manuel':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(233,140,53,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'En Pause':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 6.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(36,166,51,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_couche_piezos_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var labelFont = "18.2px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.6000000000000005;
+    var textAlign = "center";
+    var offsetX = 15;
+    var offsetY = 10;
+    var feature
+	var value
+    var clusteredFeatures = feature.get("features");
+    size = clusteredFeatures.length;
+    if (size == 1) { // If cluster has one feature
+        var feature = clusteredFeatures[0];
+        value = clusteredFeatures[0].get("Type");
+        if (feature.get("id") !== null) {
+            labelText = String(feature.get("id"));
+        }
+    } else { // If cluster has more than one feature
+		labelText = size.toString();
+		var radius = 6 + Math.log(size) * 3;
+		var maxClusterSize = 80;
+		var relativeSize = Math.min(size / maxClusterSize, 1);
+		var redComponent, greenComponent, blueComponent = 0;
+		if (relativeSize < 0.5) {
+			redComponent = Math.floor(210 * (relativeSize / 0.5));
+			greenComponent = 210;
+		} else {
+			redComponent = 210;
+			greenComponent = Math.floor(210 * (1 - (relativeSize - 0.5) / 0.5));
+		}
+		var color = `rgba(${redComponent}, ${greenComponent}, ${blueComponent}, 0.75)`;
+		return [
+			new ol.style.Style({
+				image: new ol.style.Circle({
+					radius: radius + 4,
+					fill: new ol.style.Fill({
+						color: `rgba(${redComponent}, ${greenComponent}, ${blueComponent}, 0.3)`
+					})
+				})
+			}),
+			new ol.style.Style({
+				image: new ol.style.Circle({
+					radius: radius,
+					fill: new ol.style.Fill({
+						color: color
+					})
+				}),
+				text: new ol.style.Text({
+					font: labelFont,
+					text: labelText,
+					fill: new ol.style.Fill({
+						color: labelFill
+					}),
+					placement: placement
+				})
+			})
+		];
+	}
+    
+    var style = categories_couche_piezos_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
